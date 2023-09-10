4333e75ab77a858d10ecc3526b05d345d982929c
marky376 <mmutuamark@gmail.com>
2023-09-10 09:54:27 -0700

    Take three

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return:always 0
+ */
+
+int main(void);
+{
+	char = i;
+
+	for (i = 'b'; i <= 'f'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+
+}
