d0641937159bce77d09eb5eab18384540e200e2f
marky376 <mmutuamark@gmail.com>
2023-09-10 21:28:14 -0700

    Take four

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 /**
- * main - main function
+ * main -main function
  *
  * Return:always 0
+ *
  */
 
 int main(void)
 {
-	char = i;
-	for (i = 'b'; i <= 'z'; i++)
-		putchar(i);
-	for (i = 'B'; i <= 'Z'; i++)
-		putchar(i);
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
 	putchar('\n');
+
 	return (0);
+
+
 }
+
