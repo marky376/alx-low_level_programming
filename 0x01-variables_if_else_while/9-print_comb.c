72e0a290ce5da827e5a3e1b7fc2ff497636c9cb9
marky376 <mmutuamark@gmail.com>
2023-09-11 08:36:57 -0700

    Last take on mandatory tasks

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ *main - main function
+ *
+ *Return:always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
