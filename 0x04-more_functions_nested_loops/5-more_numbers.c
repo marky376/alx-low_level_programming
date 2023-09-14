716900cae348ef94a5f7bfe3c9025978989cf13e
marky376 <mmutuamark@gmail.com>
2023-09-14 21:15:45 +0300

    Printing numbers 0 to 9 excluding 2 and 4

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - function prints 10 times the numbers from 0 to 14 followed by a new line
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for(j = 0; j <= 14; j++)
+			_putchar(j + '0');
+	}
+	_putchar('\n');
+
+}
