7831aa31b6c03b945726cdac0529e7b509a7878a
marky376 <mmutuamark@gmail.com>
2023-09-15 05:54:09 +0300

    Squaring

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,7 +14,10 @@ void print_square(int size)
 		int i, j;
 
 		for (i = 1; i <= size; i++)
-			_putchar('&');
-		_putchar('\n');
+		{
+			for (j = 1; j <= size; j++)
+				_putchar('&');
+			_putchar('\n');
+		}
 	}
 }
