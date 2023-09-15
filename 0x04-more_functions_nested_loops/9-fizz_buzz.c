3a97125a2e130b4621808fa6fb55673ba961ac7c
marky376 <mmutuamark@gmail.com>
2023-09-15 10:41:24 +0300

    The FizzBuzz project

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - main function
+ * Return: 0;
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s", "FizzBuzz");
+		else if (i % 3 == 0)
+			printf("%s", "Fizz");
+		else if (i % 5 == 0)
+			printf("%s", "Buzz");
+		else
+			printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
