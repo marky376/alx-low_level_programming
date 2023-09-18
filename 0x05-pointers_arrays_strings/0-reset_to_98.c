409f6cf892e4800080739339ce508332b5cb89b2
marky376 <mmutuamark@gmail.com>
2023-09-18 13:36:01 +0300

    Function that takes a pointer as a parameter

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,11 @@
-#include <stdio.h>
+#include "main.h"
 
-void reset_to_98(int *n) {
-    *n = 98;
-}
-
-int main() {
-	int num = 5;
-
-	printf("Before reset_to_98: num = %d\n", num);
-	reset_to_98(&num);  // Pass the address of 'num' to the function
-	printf("After reset_to_98: num = %d\n", num);
-
-	return 0;
+/**
+ * reset_to_98 - modifies the value of an integer to 98
+ * @n: a pointer to an integer
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
 
