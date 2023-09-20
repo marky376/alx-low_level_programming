f9c0072e6869a582bdcd6c8a09ae604c51773672
marky376 <135600741+marky376@users.noreply.github.com>
2023-09-20 18:32:52 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,41 +1,33 @@
 #include "main.h"
-#include <stdio.h>
-
-
 
 /**
- * _strcat - Concatenates two strings.
- * @dest: The destination string.
- * @src: The source string to be concatenated to dest.
- *
- * Return: A pointer to the resulting concatenated string (dest).
- */
+_struct - Concatenates two strings
+@dest: The destination string
+@src: The source string to be concatenated to dest
 
+Return: A pointer to the resulting string
+*/
 char *_strcat(char *dest, char *src)
 {
-	int dest_len = 0;
-	int src_len = 0;
+    int dest_len = 0;
+    int src_len = 0;
 
-	/* Find the length of the destination string */
-	while (dest[dest_len] != '\0')
-	{
-		dest_len++;
-	}
+    /* Find the length of the destination string*/
+    while (dest[dest_len] != '\0') {
+        dest_len++;
+    }
 
-	/* Find the length of the source string */
-	while (src[src_len] != '\0')
-	{
-		src_len++;
-	}
+    /* Find the length of the source string*/
+    while (src[src_len] != '\0') {
+        src_len++;
+    }
 
-	/* Append the source string to the destination */
-	for (int i = 0; i < src_len; i++)
-	{
-		dest[dest_len + i] = src[i];
-	}
+    /* Append the source string to the destination*/
+    for (int i = 0; i < src_len; i++) {
+        dest[dest_len + i] = src[i];
+    }
+/* Add the null terminator to the concatenated string*/
+    dest[dest_len + src_len] = '\0';
 
-	/* Add the null terminator to the concatenated string */
-	dest[dest_len + src_len] = '\0';
-	return dest;
+    return dest;
 }
-
