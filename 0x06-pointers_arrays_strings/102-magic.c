e07b31789f27b2fbddebaef7365df4af3991c4de
marky376 <mmutuamark@gmail.com>
2023-09-21 21:11:40 +0300

    A program that prints a[2] = 98 followed by a new line

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;/** Assign 98 to the memory location pointed to by p*/
+
+	*p = 98; 
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
