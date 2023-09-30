a73139374b0c0755f72b5a26b339a113d2382cd3
marky376 <mmutuamark@gmail.com>
2023-09-30 03:24:50 +0300

    A commit on a function that adds two numbers and shows the numbers of coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,30 +9,29 @@
  */
 int main(int argc, char *argv[])
 {
-    int num, j, result;
-    int coins[] = {25, 10, 5, 2, 1};
+	int num, k, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return 1;
-    }
-
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	num = atoi(argv[1]);
-	result = 0;
+	result = (0);
 
 	if (num < 0)
 	{
 	printf("0\n");
-	return 0;  /* Added return statement to exit early*/
+	return (0);  /* Added return statement to exit early*/
 	}
 
-    for (j = 0; j < 5 && num >= 0; j++)
-        {
-                while (num >= coins[j])
-                {
+	for (k = 0; k < 5 && num >= 0; k++)
+	{
+		while (num >= coins[k])
+		{
 			result++;
-			num -= coins[j];
+			num -= coins[k];
 		}
 	}
 		printf("%d\n", result);
