18bf89f3655f0c0693cf694a9f315cb0d9fb6d95
marky376 <mmutuamark@gmail.com>
2023-10-03 09:05:43 +0300

    A commit on a function concatenates two strings.

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - Concatenates two given strings
+ * @s1: The first string
+ * @s2: The second string
+ *
+ * Return: A pointer to a newly allocated string containing s1 followed by s2
+ *         if memory allocation fails, return NULL.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *concatenated;
+	int len1 = 0, len2 = 0, i, j;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
+	concatenated = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+
+	if (concatenated == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len1; i++)
+	{
+		concatenated[i] = s1[i];
+	}
+	for (j = 0; j < len2; j++)
+	{
+		concatenated[i + j] = s2[j];
+	}
+	concatenated[i + j] = '\0';
+
+	return (concatenated);
+}
