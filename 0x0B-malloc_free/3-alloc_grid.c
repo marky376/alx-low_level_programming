0e0daf2a19aeee867bb702f0286afac5850ec50d
marky376 <mmutuamark@gmail.com>
2023-10-03 09:19:39 +0300

    A commit on a function that returns a pointer to a 2 dimensional array of integers..

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - Allocate a 2D array of integers
+ * @width: Width of the given grid
+ * @height: Height of the given grid
+ *
+ * Return: Apointer to the allocated grid, or the NULL incase of failure
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	grid = (int **)malloc(height * sizeof(int *));
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+				return (NULL);
+			}
+		}
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
+
