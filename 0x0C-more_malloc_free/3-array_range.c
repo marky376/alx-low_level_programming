d76459ed7a09d70650b1717ee0ad067ab9dce074
marky376 <mmutuamark@gmail.com>
2023-10-05 12:48:49 +0300

    A commit on a function that creates an array of integers  .

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - Creates an array of integres from minimum to maximum
+ * @min:  The minimum value
+ * @max: The maximum value
+ *
+ *
+ * Return: A pointer to the newly created array or NULL in case of
+ * a failure.
+ */
+
+int *array_range(int min, int max)
+{
+	int *array;
+	int i, size;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1;
+
+	array = malloc(sizeof(int) * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = min++;
+	return (array);
+}
