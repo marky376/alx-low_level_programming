58a568a9ec7436c4304942a56e6d272431bb2f0f
marky376 <mmutuamark@gmail.com>
2023-10-09 12:22:05 +0300

    A commit on a function that creates a structure.

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,8 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - input for dogs information
+ * @name: Name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
