bc0fee13e7cebc5eacee497962a6177dcb957731
marky376 <mmutuamark@gmail.com>
2023-10-09 18:16:13 +0300

    A commit on a function that creates a structure.

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,4 +2,17 @@
 #include "dog.h"
 #include <stdlib.h>
 
+/**
+ * print_dog - print dog
+ * @d: dog to print
+ */
 
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Age: %f\n", (d->age) ? d->age : 0);
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
+	}
+}
