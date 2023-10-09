820430489a3271a10e7336bfd0e0bc7e6f279651
marky376 <mmutuamark@gmail.com>
2023-10-09 08:42:49 +0300

    A commit on a function that prints a struct dog followed by new lines.

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+}
