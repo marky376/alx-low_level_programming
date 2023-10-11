ebd7ac4d38f4282880c779b861726ff85f93e7b4
marky376 <mmutuamark@gmail.com>
2023-10-11 14:57:33 +0300

    A commit on a function pointer that searches for an integer.

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+/**
+ * int_index - the index of integers in an integer
+ * @array: The array of integers
+ * @size: The size of the array
+ * @cmp: the pointer to the function that compares ints
+ *
+ * Return: always -1 if the size is less than or equal to zero
+ */
+
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
@@ -13,13 +22,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	}
 
-	for (i =0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) == 1)
 		{
 			return (i);
 		}
-		
 	}
 	return (-1);
 }
