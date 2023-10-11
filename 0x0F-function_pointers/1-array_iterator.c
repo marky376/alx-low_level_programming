6676055c55999f6aa67605bc462fcf41409b49a7
marky376 <mmutuamark@gmail.com>
2023-10-11 14:13:59 +0300

    A commit on a function pointer that executes a function given as a parameter on each elemnt of an array .

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+	
+}
