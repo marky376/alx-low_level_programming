8b3ff6641723b5be1ae4f60c0c08ca7d0a44fa2b
marky376 <mmutuamark@gmail.com>
2023-10-20 05:40:01 +0300

    A commit on a function that prints a string before the main function is executed.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * print_before_main - prints an output before the main function is executed
+ */
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_before_main(void);
+int main(void)
+{
+	print_before_main();
+    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+    return (0);
+}
