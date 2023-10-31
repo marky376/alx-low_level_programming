273e2bb28befe7ff59332c34617215e16e9c57a0
marky376 <mmutuamark@gmail.com>
2023-10-31 04:59:53 +0300

    A commit on a function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file with certain permissions
+ * @filename: name of the file
+ * @text_content: A null terminated string to write to the file
+ * 
+ * Return: 1 on success and -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fptr, fpot;
+	mode_t mode = S_IRUSR | S_IWUSR;/*Permissions for read and write*/
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fptr = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (fptr == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		fpot = write(fptr, text_content, strlen(text_content));
+		if (fpot == -1)
+		{
+			close(fptr);
+			return (-1);
+		}
+	}
+
+	close(fptr);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
