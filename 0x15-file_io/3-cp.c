93f31ce6a94c9b9728cbe6a8bbc7f5f7d5f2da83
marky376 <mmutuamark@gmail.com>
2023-10-31 06:08:44 +0300

    A commit on a function that copies contents  in a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,68 +8,72 @@
 
 #define BUFFER_SIZE 1024
 
+/**
+ * main - Copies the content of one file to another.
+ * @argc: The argument count.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success, other values on failure.
+ */
 int main(int argc, char *argv[])
 {
 	char buffer[BUFFER_SIZE];
-	ssize_t bread, bwrite;
+    	ssize_t bytes_read, bytes_written;
 	const char *file_from = argv[1];
 	const char *file_to = argv[2];
-	int fptr_from, fptr_to;
+	int fd_from, fd_to;
 
 	if (argc != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	fptr_from = open(file_from, O_RDONLY);
-	if (fptr_from == -1)
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
 
-	fptr_to = open(file_to, O_WRONLY | O_CREATE | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-	if (fptr_to == -1)
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (fd_to == -1)
 	{
 		dprintf(2, "Error: Can't write to file %s\n", file_to);
-		close(fptr_from);
+		close(fd_from);
 		exit(99);
 	}
 
-	while ((bread = read(fptr_from, buffer, BUFFER_SIZE)) > 0)
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		bwrite = write(fptr_to, buffer, bread);
-
-		if (bwrite == -1)
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1)
 		{
 			dprintf(2, "Error: Can't write to file %s\n", file_to);
-			close(fptr_from);
-			close(fptr_to);
+			close(fd_from);
+			close(fd_to);
 			exit(99);
 		}
 	}
 
-	if (bread == -1)
-    	{
+	if (bytes_read == -1)
+	{
 		dprintf(2, "Error: Can't read from file %s\n", file_from);
-		close(fptr_from);
-		close(fptr_to);
+		close(fd_from);
+		close(fd_to);
 		exit(98);
 	}
 
-	if (close(fptr_from) == -1)
-    	{
-		dprintf(2, "Error: Can't close fd %d\n", fptr_from);
+	if (close(fd_from) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
 
-	if (close(fptr_to) == -1)
-    	{
-	dprintf(2, "Error: Can't close fd %d\n", fptr_to);
+	if (close(fd_to) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
 
-	return 0;
+	return (0);
 }
-
