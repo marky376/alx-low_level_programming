c7731470c98a743f4b700ce146aac90f8f5c48f2
marky376 <mmutuamark@gmail.com>
2023-10-31 05:51:39 +0300

    A commit on a function that copies contents  in a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,25 +11,25 @@
 int main(int argc, char *argv[])
 {
 	char buffer[BUFFER_SIZE];
-        ssize_t bread, bwrite;
+	ssize_t bread, bwrite;
 	const char *file_from = argv[1];
-        const char *file_to = argv[2];
+	const char *file_to = argv[2];
 	int fptr_from, fptr_to;
 
 	if (argc != 3)
-    	{
+	{
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
 	fptr_from = open(file_from, O_RDONLY);
-    	if (fptr_from == -1)
-    	{
+	if (fptr_from == -1)
+	{
 		dprintf(2, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
 
-	fptr_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	fptr_to = open(file_to, O_WRONLY | O_CREATE | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 	if (fptr_to == -1)
 	{
 		dprintf(2, "Error: Can't write to file %s\n", file_to);
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 	while ((bread = read(fptr_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bwrite = write(fptr_to, buffer, bread);
-        
+
 		if (bwrite == -1)
 		{
 			dprintf(2, "Error: Can't write to file %s\n", file_to);
