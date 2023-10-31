6f374ff76f092e8e87fd2f4d2e52658097ff550c
marky376 <mmutuamark@gmail.com>
2023-10-31 04:40:04 +0300

    A commit on a function that reads a file and outputs it to the POSIX standard input.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,62 @@
+#include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - reads a text file and prints it to thePOSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
+ *
+ * Return: if NULL, 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fptr;
+	ssize_t bread, bwrite;
+	char *buffer = NULL;
+
+	if (filename == NULL)
+		return (0);
+	fptr = open(filename, O_RDONLY);
+
+	if (fptr == -1)
+	{	
+		free(buffer);	
+		return (0);
+	}
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		return (0);
+	}
+
+	if (buffer == NULL)
+	{
+		close(fptr);
+		return (0);
+	}
+
+	bread = read(fptr, buffer, letters);
+	close(fptr);
+
+	if (bread == -1)
+	{
+		free(buffer);
+		close(fptr);
+		return (0);
+	}
+
+	bwrite = write(STDOUT_FILENO, buffer, bread);
+	if (bwrite == -1 || bwrite != bread)
+	{
+		free(buffer);
+		close(fptr);
+		return (0);
+	}
+	free(buffer);
+	close(fptr);
+
+	return (bread);
+}
