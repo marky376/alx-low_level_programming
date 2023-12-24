d142b266671fd879e499e6aa872a3d20699d12d5
marky376 <mmutuamark@gmail.com>
2023-12-24 22:12:03 +0300

    A commit on a hash function that retrieves a value associated with a key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - function that retrieves a value associated with a key
+ * @ht: is the hash table you want to look into
+ * @key: is the key you are looking for
+ * Return: the value associated, or NULL if key couldn't be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (!ht || !key )
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
+}
