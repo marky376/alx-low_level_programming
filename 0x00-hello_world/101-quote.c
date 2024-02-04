83ee8075471dd49d18d11756df9cd26104bbfad3
marky376 <mmutuamark@gmail.com>
2024-02-04 17:39:04 +0300

    Adding comments on files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,22 @@
 #include <unistd.h>
 
+/**
+ * echo_err - send text to standard error (stderr)
+ *
+ * @s: string to be sent to stderr
+ *
+ * Return: number of characters sent to stderr
+ */
 int echo_err(const char *s);
 
 /**
- * main - driver
+ * main - Entry point of the program
  *
- * Return: 1
+ * Return: Always 1 (indicating an error)
  */
 int main(void)
 {
+	// Call echo_err function with the specified string
 	echo_err("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 
 	return (1);
@@ -17,7 +25,7 @@ int main(void)
 /**
  * echo_err - send text to standard error (stderr)
  *
- * @s: string
+ * @s: string to be sent to stderr
  *
  * Return: number of characters sent to stderr
  */
@@ -25,8 +33,11 @@ int echo_err(const char *s)
 {
 	int i = 0;
 
+	// Iterate through each character of the string
 	for (; *s; s++)
 		i += write(2, s, 1);
+
+	// Write a newline character to stderr
 	write(2, "\n", 1);
 
 	return (i);
