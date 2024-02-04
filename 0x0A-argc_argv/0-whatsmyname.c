42f3c46036bd6ff76f5b7497031a98948e089b15
marky376 <mmutuamark@gmail.com>
2024-02-04 17:41:59 +0300

    Adding comments on files

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,17 @@
-
 #include <stdio.h>
 
 /**
  * main - Entry point
- * @argc: integer
- * @argv: string
- * Return: always 0
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the command-line arguments
+ *
+ * Description: This program prints the name of the program itself.
+ *
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-		if (argc > 0)
+	if (argc > 0)
 		printf("%s\n", argv[0]);
-		return (0);
+	return (0);
 }
