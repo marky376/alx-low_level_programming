c1be0a4b0006c71dda3b1352cdf89788c7cde2b5
marky376 <mmutuamark@gmail.com>
2024-02-04 17:47:05 +0300

    Adding comments on files

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,30 +2,36 @@
 #include <stdio.h>
 
 /**
- * main - main entry
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: always 0
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the command-line arguments
+ *
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
 	int num, k, result;
 	int coins[] = {25, 10, 5, 2, 1};
 
+	// Check if the number of command-line arguments is correct
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	num = atoi(argv[1]);
-	result = (0);
 
+	// Convert the second command-line argument to an integer
+	num = atoi(argv[1]);
+	result = 0;
+
+	// Check if the number is negative
 	if (num < 0)
 	{
-	printf("0\n");
-	return (0);  /* Added return statement to exit early*/
+		printf("0\n");
+		return (0);
 	}
 
+	// Calculate the minimum number of coins needed to make change
 	for (k = 0; k < 5 && num >= 0; k++)
 	{
 		while (num >= coins[k])
@@ -34,6 +40,8 @@ int main(int argc, char *argv[])
 			num -= coins[k];
 		}
 	}
-		printf("%d\n", result);
-		return (0);
+
+	// Print the result
+	printf("%d\n", result);
+	return (0);
 }
