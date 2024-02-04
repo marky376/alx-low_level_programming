c2076692a41875558d648112093b638f2d589a4c
marky376 <mmutuamark@gmail.com>
2024-02-04 17:35:16 +0300

    Adding comments on files

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,10 @@
 /**
  * main - main function
  *
+ * This is the main function of the program.
+ * It prints the message "with proper grammar, but the outcome is a piece of art,"
+ * and returns 0.
+ *
  * Return: 0
  */
 int main(void)
