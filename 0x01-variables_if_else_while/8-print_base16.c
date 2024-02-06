c417ef7e550c1ffe6d82c0defeba88003403ca2b
marky376 <mmutuamark@gmail.com>
2024-02-06 05:49:31 +0300

    Adding comments

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,26 @@
 #include<stdio.h>
-/**  main - main function 
- * Return:always 0
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the numbers from 0 to 9
+ *              and the lowercase letters from a to f.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
 	char j;
 
+	// Print numbers from 0 to 9
 	for (i = 0; i < 10; i++)
-		putchar(i + 0);
+		putchar(i + '0');
+
+	// Print lowercase letters from a to f
 	for (j = 'a'; j <= 'f'; j++)
 		putchar(j);
+
 	putchar('\n');
 	return (0);
 }
