1be7ce0a50e9c018fa95d3c6ec28278b56455568
marky376 <mmutuamark@gmail.com>
2024-02-06 05:17:09 +0300

    Adding comments

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,6 +1,18 @@
+/**
+ * @file 4-sum.h
+ * @brief Header file containing the SUM macro.
+ */
+
 #ifndef SUM_H
 #define SUM_H
 
+/**
+ * @def SUM(x, y)
+ * Computes the sum of two numbers.
+ * @param x The first number.
+ * @param y The second number.
+ * @return The sum of x and y.
+ */
 #define SUM(x, y) ((x) + (y))
 
 #endif
