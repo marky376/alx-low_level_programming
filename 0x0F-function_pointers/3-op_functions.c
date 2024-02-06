b8bda9a7d4ea85bbfb367b52eca71392dd88185a
marky376 <mmutuamark@gmail.com>
2024-02-06 05:35:17 +0300

    Adding comments

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,3 +1,44 @@
+/**
+ * op_add - performs the addition of two given numbers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The sum of the two given numbers.
+ */
+
+/**
+ * op_sub - gets the difference of two given numbers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The difference of the two given numbers.
+ */
+
+/**
+ * op_div - returns the result of the division of two numbers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of the division of the two given numbers.
+ *         If the second integer is 0, it prints an error message and exits with status 99.
+ */
+
+/**
+ * op_mod - returns the remainder of the division of two numbers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The remainder of the division of the two given numbers.
+ *         If the second integer is 0, it prints an error message and exits with status 99.
+ */
+
+/**
+ * op_mul - returns the multiplication of two given numbers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The multiplication of the two given numbers.
+ */
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
