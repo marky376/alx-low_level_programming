185f5a7fe73efada20eb18d42fa183c7f553354d
marky376 <mmutuamark@gmail.com>
2024-02-06 05:34:45 +0300

    Adding comments

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,17 +1,18 @@
-#include <stdio.h>
-#include "3-calc.h"
-#include <stdlib.h>
-
 /**
- * get_op_func - selects the correct function to pe
- * rform the operation in question
+ * get_op_func - selects the correct function to perform the operation in question
  * @s: operator passed as an argument to the program
+ *
+ * This function takes an operator as input and returns a pointer to the function
+ * that is linked to the operator. It searches through an array of structs, `ops`,
+ * which contains operator-function pairs. If a matching operator is found, the
+ * corresponding function is returned. If no matching operator is found, an error
+ * message is printed and the program exits with status code 99.
  *
  * Return: a pointer to the function that is linked to the operator
  */
-
 int (*get_op_func(char *s))(int, int)
 {
+	// Array of operator-function pairs
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -21,17 +22,18 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}
 	};
 
-	int i;
+	int i = 0;
 
-	i = 0;
-
+	// Iterate through the array of operator-function pairs
 	while (ops[i].op)
 	{
+		// Check if the current operator matches the input operator
 		if (*s == *ops[i].op && s[1] == '\0')
 			return (ops[i].f);
 		i++;
 	}
 
+	// If no matching operator is found, print an error message and exit
 	printf("Error\n");
 	exit(99);
 }
