bef7177404b176f9e55127bc9bfd996c0f2d3b1a
marky376 <mmutuamark@gmail.com>
2024-02-06 05:13:58 +0300

    Adding comments

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,14 @@
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
 
+/**
+ * @file 0-object_like_macro.h
+ * @brief Header file that defines a macro for size.
+ *
+ * This header file defines a macro named SIZE which represents the size 1024.
+ * It is used to provide a symbolic representation of a fixed value in the code.
+ */
+
 #define SIZE 1024
 
 #endif
