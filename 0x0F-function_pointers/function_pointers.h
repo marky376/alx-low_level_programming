a6c57b699670f4052df65f1adf52d48d8ad3a9eb
marky376 <mmutuamark@gmail.com>
2024-02-06 05:36:19 +0300

    Adding comments

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,20 @@
+// This is a header file named "function_pointers.h"
+// It contains function prototypes for function pointer usage
+
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+
+// Function prototype for print_name function
+// Takes a string pointer and a function pointer as parameters
 void print_name(char *name, void (*f)(char *));
+
+// Function prototype for array_iterator function
+// Takes an integer array, size of the array, and a function pointer as parameters
 void array_iterator(int *array, size_t size, void (*action)(int));
+
+// Function prototype for int_index function
+// Takes an integer array, size of the array, and a function pointer as parameters
+// Returns the index of the first element for which the comparison function returns non-zero
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
