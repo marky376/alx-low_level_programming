2afd8f8f99c47b05b2ac7052da8c251f957f5da1
marky376 <mmutuamark@gmail.com>
2024-02-06 05:50:56 +0300

    Adding comments

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,29 @@
 #include<stdio.h>
+
 /**
- *main - main function
+ * main - Entry point of the program
  *
- *Return:always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
 
+	// Loop through numbers from 0 to 9
 	for (i = 0; i < 10; i++)
 	{
-		if (i !9)
-		{	
+		// Check if the number is not equal to 9
+		if (i != 9)
+		{
+			// Print the number followed by a comma and a space
 			putchar(i + '0');
 			putchar(',');
 			putchar(' ');
-		}	
+		}
 	}
+
+	// Print a new line character
 	putchar('\n');
+
 	return (0);
 }
