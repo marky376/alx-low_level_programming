b78a2c1ad64e5c5180466194821fd9fab3681d09
marky376 <mmutuamark@gmail.com>
2024-02-06 05:57:47 +0300

    Adding comments

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,24 +1,37 @@
 #include<stdio.h>
+
 /**
- * main - main function
+ * main - Entry point of the program
  *
- * Return:always 0
+ * Description: This program prints all possible combinations of two two-digit numbers
+ *              without repeating any combination.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i, j;
 
+	// Loop through all possible values of i from 0 to 99
 	for (i = 0; i < 100; i++)
 	{
+		// Loop through all possible values of j from 0 to 99
 		for (j = 0; j < 100; j++)
 		{
+			// Check if i is less than j to avoid repeating combinations
 			if (i < j)
 			{
+				// Print the first two digits of i
 				putchar((i / 10) + 48);
+				// Print the last two digits of i
 				putchar((i % 10) + 48);
 				putchar(' ');
+				// Print the first two digits of j
 				putchar((j / 10) + 48);
+				// Print the last two digits of j
 				putchar((j % 10) + 48);
+
+				// Check if it is not the last combination
 				if (i != 98 || j != 99)
 				{
 					putchar(',');
