5fa8d8db1e76a208f8f8bf01cd8b4f447e804075
marky376 <mmutuamark@gmail.com>
2024-02-06 05:55:32 +0300

    Adding comments

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,23 +1,32 @@
 #include<stdio.h>
+
 /**
- * main - main function
+ * main - Entry point of the program
  *
- * Return:always 0
+ * This program prints all possible combinations of three digits,
+ * without repetition, separated by a comma and a space.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-
 	int i, j, k;
 
+	// Loop through all possible values for the first digit (i)
 	for (i = 48; i <= 55; i++)
 	{
+		// Loop through all possible values for the second digit (j)
 		for (j = i + 1; j <= 56; j++)
 		{
+			// Loop through all possible values for the third digit (k)
 			for (k = j + 1; k <= 57; k++)
 			{
+				// Print the three digits
 				putchar(i);
 				putchar(j);
 				putchar(k);
+
+				// Add comma and space if i is less than 55
 				if (i < 55)
 				{
 					putchar(',');
@@ -26,6 +35,8 @@ int main(void)
 			}
 		}
 	}
+
+	// Print a new line
 	putchar('\n');
 
 	return (0);
