deb2886e4e949306d6cbcf98bba5a79b0c0d1d53
marky376 <mmutuamark@gmail.com>
2024-02-08 22:50:08 +0300

    base_model.py

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,19 +1,25 @@
-#include "main.h"
-
 /**
  * rev_string - Reverses a string
  * @s: Input string
- * Return: String in reverse
+ *
+ * Description:
+ * This function takes a string as input and reverses it in place.
+ * It swaps the characters from the beginning and end of the string
+ * until the middle is reached, effectively reversing the string.
+ *
+ * Return: void
  */
-
 void rev_string(char *s)
 {
 	char rev = s[0];
 	int counter = 0;
 	int i;
 
+	// Find the length of the string
 	while (s[counter] != '\0')
-	counter++;
+		counter++;
+
+	// Swap characters from the beginning and end of the string
 	for (i = 0; i < counter; i++)
 	{
 		counter--;
@@ -22,4 +28,3 @@ void rev_string(char *s)
 		s[counter] = rev;
 	}
 }
-
