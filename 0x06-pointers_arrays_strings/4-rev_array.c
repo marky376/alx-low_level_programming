63740c547e4803132d067d33b98c1ce4ea8a6de6
marky376 <mmutuamark@gmail.com>
2024-02-08 23:06:48 +0300

    base_model.py

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,6 +4,10 @@
  * reverse_array - Reverses the content of an array of integers
  * @a: Pointer to the array
  * @n: Number of elements in the array
+ *
+ * This function takes an array of integers and reverses its content.
+ * It uses a two-pointer approach, swapping the elements from the beginning
+ * and end of the array until they meet in the middle.
  */
 void reverse_array(int *a, int n)
 {
