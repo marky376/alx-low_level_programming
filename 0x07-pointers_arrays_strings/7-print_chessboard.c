0aa9962ec3de8085d46053b6a9eb810ad4ed608a
marky376 <mmutuamark@gmail.com>
2024-02-08 23:21:15 +0300

    base_model.py

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,8 +4,12 @@
 /**
  * print_chessboard - prints a chessboard
  * @a: the 2D array representing the chessboard
+ *
+ * This function takes a 2D array representing a chessboard and prints it to the console.
+ * Each element of the array represents a square on the chessboard, and the function
+ * prints the characters in the array to represent the chessboard.
+ * The chessboard is printed as an 8x8 grid, with each row separated by a newline character.
  */
-
 void print_chessboard(char(*a)[8])
 {
 	int i, j;
