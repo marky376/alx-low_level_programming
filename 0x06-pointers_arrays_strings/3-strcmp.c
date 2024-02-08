2b1997ee14630af8bc467a2c56e69a6a43ed977f
marky376 <mmutuamark@gmail.com>
2024-02-08 23:06:05 +0300

    base_model.py

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,21 +1,13 @@
-#include "main.h"
-
 /**
  * _strcmp - Compares two strings
  * @s1: The first string to compare
  * @s2: The second string to compare
  *
- * Return: 0 if s1 and 1 if s2
+ * This function compares the characters of two strings, s1 and s2, until a
+ * difference is found or the end of either string is reached. It returns an
+ * integer value indicating the difference between the first differing
+ * characters in the two strings.
+ *
+ * Return: 0 if the strings are equal, a negative value if s1 is less than s2,
+ *         or a positive value if s1 is greater than s2.
  */
-
-int _strcmp(char *s1, char *s2)
-{
-	while (*s1 && (*s1 == *s2))
-	{
-		s1++;
-		s2++;
-	}
-
-	return (*s1 - *s2);
-
-}
