a0b55fdd4fdbcbb9654eff91575352a5ea187a60
marky376 <mmutuamark@gmail.com>
2024-02-08 23:19:33 +0300

    base_model.py

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <stddef.h>/*include the <stddef.h> for the NULL used in the program*/
+
 /**
  * _strchr - locate a character in a string
  * @s: pointer to the string to search
  * @c: the character to locate
  *
- * Return: a pointer to the 1st occurence of char c in the string
- * or NULL if the character is not found.
+ * This function searches for the first occurrence of the character 'c' in the string 's'.
+ * It returns a pointer to the first occurrence of 'c' in 's', or NULL if 'c' is not found.
  */
 
 char *_strchr(char *s, char c)
