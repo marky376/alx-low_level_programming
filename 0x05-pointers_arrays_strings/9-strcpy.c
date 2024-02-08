6e062b8a7ef8c873fc172ddc3773c213fe880233
marky376 <mmutuamark@gmail.com>
2024-02-08 22:55:35 +0300

    base_model.py

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,25 +1,30 @@
-#include "main.h"
-
 /**
- * char *_strcpy - a function that copies the string pointed to by src
- * @dest: copy to
- * @src: copy from
- * Return: string
+ * _strcpy - Copies the string pointed to by src to dest.
+ *
+ * @dest: Pointer to the destination string.
+ * @src: Pointer to the source string.
+ *
+ * Return: Pointer to the destination string.
  */
 char *_strcpy(char *dest, char *src)
 {
 	int l = 0;
 	int x = 0;
 
+	// Calculate the length of the source string
 	while (*(src + l) != '\0')
 	{
 		l++;
 	}
-	for ( ; x < l ; x++)
+
+	// Copy each character from source to destination
+	for (; x < l; x++)
 	{
 		dest[x] = src[x];
 	}
-	dest[l] = '\0';
-	return (dest);
-}
 
+	// Add null terminator to the destination string
+	dest[l] = '\0';
+
+	return dest;
+}
