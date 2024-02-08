356c1efbafe9b96efcb4f2416f67f3486414d38c
marky376 <mmutuamark@gmail.com>
2024-02-08 23:07:32 +0300

    base_model.py

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,3 +1,14 @@
+/**
+ * string_toupper - function that changes lowercase letters to uppercase in a string
+ * @str: the input string
+ *
+ * This function takes a string as input and converts all lowercase letters to uppercase.
+ * It iterates through each character in the string and checks if it is a lowercase letter.
+ * If it is, it converts it to uppercase by subtracting the difference between 'a' and 'A'.
+ * The modified string is then returned.
+ *
+ * Return: A pointer to the modified string.
+ */
 #include "main.h"
 
 /**
