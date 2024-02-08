5c3c461f79b6c44c041419dcf2c045de67340d84
marky376 <mmutuamark@gmail.com>
2024-02-08 23:09:32 +0300

    base_model.py

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,31 +1,6 @@
-#include "main.h"
-
 /**
  * rot13 - Encodes a string using rot13
  * @s: The string to encode
  *
  * Return: A pointer to the encoded string
  */
-
-char *rot13(char *s)
-{
-	char *ptr = s;
-	char rot13_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char rot13_map[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	int i;
-
-	while (*s)
-	{
-		for (i = 0; rot13_chars[i]; i++)
-		{
-			if (*s == rot13_chars[i])
-			{
-				*s = rot13_map[i];
-				break;
-			}
-		}
-		s++;
-	}
-
-	return (ptr);
-}
