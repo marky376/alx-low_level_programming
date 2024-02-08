fb5cd53af996c8624709f3956b3c57d99e59fad7
marky376 <mmutuamark@gmail.com>
2024-02-08 23:08:56 +0300

    base_model.py

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,29 +1,12 @@
-#include "main.h"
-
 /**
  * leet - encodes a string into 1337
  * @str: input string to encode
  *
+ * This function takes a string as input and encodes it into "1337" or "leet" format.
+ * It replaces certain characters in the string with their corresponding leet replacements.
+ * The characters 'A' and 'a' are replaced with '4', 'E' and 'e' are replaced with '3',
+ * 'O' and 'o' are replaced with '0', 'T' and 't' are replaced with '7', and 'L' and 'l'
+ * are replaced with '1'.
+ *
  * Return: a pointer to the resulting encoded string
  */
-
-char *leet(char *str)
-{
-	int i, j;
-	char leet[] = "AaEeOoTtLl";
-	char leet_replace[] = "4433007711";
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		for (j = 0; leet[j] != '\0'; j++)
-		{
-			if (str[i] == leet[j])
-			{
-				str[i] = leet_replace[j];
-				break;
-			}
-		}
-	}
-
-	return (str);
-}
