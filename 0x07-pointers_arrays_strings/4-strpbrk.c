d2a067eef8535f23fe33b73018402be736b61262
marky376 <mmutuamark@gmail.com>
2024-02-08 23:20:23 +0300

    base_model.py

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,28 +1,10 @@
-#include "main.h"
-#include <stdlib.h>
-
 /**
  * _strpbrk - searches a string for any of a set of bytes
  * @s: pointer to the string to search
  * @accept: pointer to the string containing chars to search for
  *
- * Return: a pointer to the byte in s that matches one of the bytes in accept
- * or NULL if no such byte is found.
+ * This function searches the string pointed to by 's' for the first occurrence
+ * of any character in the string pointed to by 'accept'. It returns a pointer
+ * to the byte in 's' that matches one of the bytes in 'accept', or NULL if no
+ * such byte is found.
  */
-
-char *_strpbrk(char *s, char *accept)
-{
-	while (*s)
-	{
-		char *a = accept;
-
-		while (*a)
-		{
-			if (*s == *a)
-				return (s);
-			a++;
-		}
-		s++;
-	}
-	return (NULL);
-}
