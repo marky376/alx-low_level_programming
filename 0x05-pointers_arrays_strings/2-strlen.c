0b263375dc19a1d94db9070f3a91df7a67b9ea72
marky376 <mmutuamark@gmail.com>
2024-02-08 22:46:49 +0300

    base_model.py

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,10 @@
-#include "main.h"
-
 /**
  * _strlen - returns the length of a string
  * @s: pointer to the string
+ *
+ * This function takes a pointer to a string and returns the length of the string.
+ * It iterates through the string until it reaches the null character ('\0') and
+ * counts the number of characters encountered.
  *
  * Return: length of the string
  */
--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,9 +2,13 @@
 
 /**
  * _puts - prints a string followed by a new line
- * @str: pointers to the string to be printed
+ * @str: pointer to the string to be printed
+ *
+ * Description:
+ * The _puts function takes a pointer to a string and prints the string
+ * character by character until it reaches the null terminator '\0'.
+ * After printing the string, it prints a new line character '\n'.
  */
-
 void _puts(char *str)
 {
 	while (*str != '\0')
