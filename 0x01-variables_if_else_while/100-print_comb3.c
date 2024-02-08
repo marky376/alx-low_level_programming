2af3b42d4eb33b822b9d6d595a3375475df6ba16
marky376 <mmutuamark@gmail.com>
2024-02-08 23:17:15 +0300

    base_model.py

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,19 +1,28 @@
 #include<stdio.h>
+
 /**
- * main - main function
+ * main - Entry point of the program
  *
- * Return:always 0
+ * Description: This program prints all possible combinations of two digits
+ *              without repetition.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i, j;
 
+	// Loop through all possible first digits (from 0 to 8)
 	for (i = 48; i <= 56; i++)
 	{
+		// Loop through all possible second digits (from i+1 to 9)
 		for (j = i + 1; j <= 57; j++)
 		{
+			// Print the two digits
 			putchar(i);
 			putchar(j);
+
+			// If the first digit is less than 8, print a comma and a space
 			if (i < 56)
 			{
 				putchar(',');
@@ -22,6 +31,7 @@ int main(void)
 		}
 	}
 
+	// Print a new line
 	putchar('\n');
 
 	return (0);
