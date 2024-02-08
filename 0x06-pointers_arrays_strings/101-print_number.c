c80b97cca2d5e0b3f38d5b869779abbdf8d096d3
marky376 <mmutuamark@gmail.com>
2024-02-08 23:10:01 +0300

    base_model.py

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,3 +1,14 @@
+/**
+ * print_number - Prints an integer
+ * @n: The integer to be printed
+ *
+ * Description:
+ * This function takes an integer as input and prints it to the standard output.
+ * If the integer is negative, it is printed with a negative sign.
+ * The function uses recursion to print each digit of the number.
+ * The digits are printed in reverse order.
+ * The function assumes that the input integer is within the range of an unsigned int.
+ */
 #include "main.h"
 
 /**
