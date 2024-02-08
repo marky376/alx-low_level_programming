df2163c443ccf1b438b896eabe2b9dcb91fe1483
marky376 <mmutuamark@gmail.com>
2024-02-08 23:11:36 +0300

    base_model.py

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - function main
- * Return:always 0
+ * main - Entry point of the program
+ *
+ * This function initializes an integer array 'a' of size 5 and a pointer 'p'.
+ * It assigns the value 1024 to the element at index 2 of array 'a'.
+ * It then assigns the memory address of variable 'n' to pointer 'p'.
+ * Finally, it assigns the value 98 to the memory location pointed to by 'p'.
+ * The value of the element at index 2 of array 'a' is printed.
+ *
+ * Return: Always 0 (Success)
  */
-
-
 int main(void)
 {
 	int n;
@@ -13,8 +18,7 @@ int main(void)
 	int *p;
 
 	a[2] = 1024;
-	p = &n;/** Assign 98 to the memory location pointed to by p*/
-
+	p = &n;
 	*p = 98;
 	printf("a[2] = %d\n", a[2]);
 	return (0);
