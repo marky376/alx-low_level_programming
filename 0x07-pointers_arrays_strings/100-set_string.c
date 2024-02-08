5e3e43f1c07eed4d8acf2398ad6598b894efbbce
marky376 <mmutuamark@gmail.com>
2024-02-08 23:22:05 +0300

    base_model.py

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,12 +1,10 @@
-#include "main.h"
-
 /**
  * set_string - sets the value of a pointer to a char
  * @s: pointer to a pointer to a char (double pointer)
  * @to: pointer to the string to set the pointer to
+ *
+ * Description:
+ *   This function takes a double pointer to a char and a pointer to a string.
+ *   It sets the value of the double pointer to the address of the string.
+ *   This effectively changes the value of the original pointer to point to a new string.
  */
-
-void set_string(char **s, char *to)
-{
-	*s = to;
-}
