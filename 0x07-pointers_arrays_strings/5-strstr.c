dd701942182e783d1f446001c2bbf1dee817bcf7
marky376 <mmutuamark@gmail.com>
2024-02-08 23:20:50 +0300

    base_model.py

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,35 +1,13 @@
-#include "main.h"
-#include <stdlib.h>
-
 /**
  * _strstr - locates a substring in a string
  * @haystack: pointer to the string to search in
  * @needle: pointer to the substring to locate
  *
+ * Description:
+ * The _strstr function searches for the first occurrence of the substring
+ * needle in the string haystack. It returns a pointer to the beginning of the
+ * located substring, or NULL if the substring is not found.
+ *
  * Return: a pointer to the beginning of the located substring, or NULL if the
  * substring is not found.
  */
-
-char *_strstr(char *haystack, char *needle)
-{
-	char *h, *n;
-
-	while (*haystack)
-	{
-		h = haystack;
-		n = needle;
-
-		while (*n && (*h == *n))
-		{
-			h++;
-			n++;
-		}
-
-		if (*n == '\0')
-			return (haystack);
-
-		haystack++;
-	}
-
-	return (NULL);
-}
