0d663a2d609d71799c38d83fe6fb299a33584195
marky376 <mmutuamark@gmail.com>
2024-02-10 10:14:32 +0300

    Adding comments

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -6,6 +6,9 @@
  * @src: pointer to the source memory area
  * @n: number of bytes to copy
  *
+ * Description: This function copies 'n' bytes from the memory area pointed to by 'src'
+ *              to the memory area pointed to by 'dest'. It returns a pointer to 'dest'.
+ *
  * Return: a pointer to dest
  */
 
