7d8c5e0fbce296acaabb28edc5bd8623f97b7893
marky376 <mmutuamark@gmail.com>
2024-02-10 10:12:29 +0300

    Adding comments

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,15 +1,13 @@
-#include "main.h"
-
 /**
- * _isupper - uppercase letters
- * @c: char to check
+ * _isupper - checks if a character is an uppercase letter
+ * @c: the character to check
  *
- * Return: 0 or 1
+ * Return: 1 if the character is an uppercase letter, 0 otherwise
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	return (1);
+		return (1);
 	else
-	return (0);
+		return (0);
 }
