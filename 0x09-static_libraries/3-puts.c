d198626ce2cdbe7b46ca028c9f0ba78d7d71453a
marky376 <mmutuamark@gmail.com>
2024-02-10 10:17:09 +0300

    Adding comments

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -2,9 +2,14 @@
 
 /**
  * _puts - prints a string followed by a new line
- * @str: pointers to the string to be printed
+ * @str: pointer to the string to be printed
+ *
+ * Description:
+ * The _puts function takes a pointer to a string and prints it to the standard output,
+ * followed by a new line character. It iterates through the characters of the string
+ * until it reaches the null terminator ('\0') and writes each character to the standard output.
+ * Finally, it writes a new line character to the standard output.
  */
-
 void _puts(char *str)
 {
 	while (*str != '\0')
