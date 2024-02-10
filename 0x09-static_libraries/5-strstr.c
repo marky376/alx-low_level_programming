c01558362bb5d0496c6083c4c5fa6a6f96e076a0
marky376 <mmutuamark@gmail.com>
2024-02-10 10:19:35 +0300

    Adding comments

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -6,8 +6,14 @@
  * @haystack: pointer to the string to search in
  * @needle: pointer to the substring to locate
  *
- * Return: a pointer to the beginning of the located substring, or NULL if the
- * substring is not found.
+ * Description:
+ * The _strstr() function locates the first occurrence of the substring
+ * needle in the string haystack. The terminating null bytes ('\0') are not
+ * compared.
+ *
+ * Return:
+ * - a pointer to the beginning of the located substring, if the substring is found.
+ * - NULL, if the substring is not found.
  */
 
 char *_strstr(char *haystack, char *needle)
