6c6a2624853c21fcf4d633750c776e9456ed46d3
marky376 <mmutuamark@gmail.com>
2024-02-10 10:17:36 +0300

    Adding comments

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,13 +1,19 @@
-#include "main.h"
-
 /**
  * _strcmp - Compares two strings
  * @s1: The first string to compare
  * @s2: The second string to compare
  *
- * Return: 0 if s1 and 1 if s2
+ * Description:
+ * This function compares two strings, character by character, until a
+ * difference is found or the end of one of the strings is reached.
+ *
+ * Return:
+ * - 0 if the strings are equal
+ * - A positive value if the first differing character in s1 is greater than
+ *   the corresponding character in s2
+ * - A negative value if the first differing character in s1 is less than the
+ *   corresponding character in s2
  */
-
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 && (*s1 == *s2))
@@ -17,5 +23,4 @@ int _strcmp(char *s1, char *s2)
 	}
 
 	return (*s1 - *s2);
-
 }
