a173823ddf75a435c8472780b9060ea7dd777fe4
marky376 <mmutuamark@gmail.com>
2024-02-10 10:13:16 +0300

    Adding comments

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -6,6 +6,10 @@
  * @b: the constant byte to fill the memory with
  * @n: the number of bytes to be filled
  *
+ * Description:
+ * The _memset function fills the first n bytes of the memory area pointed to by s
+ * with the constant byte b.
+ *
  * Return: a pointer to the memory area s
  */
 
