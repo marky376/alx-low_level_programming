c1334f20b82a3499c9f7b15650e90b8a673b461e
marky376 <mmutuamark@gmail.com>
2024-02-10 10:20:03 +0300

    Adding comments

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,7 +1,13 @@
 /**
  * _abs - gets the integer's absolute value
  * @n: number
- * Return: absolute integer
+ *
+ * Description:
+ * The _abs function takes an integer as a parameter and returns its absolute value.
+ * If the number is negative, it returns the negation of the number.
+ * If the number is positive or zero, it returns the number itself.
+ *
+ * Return: The absolute value of the given integer.
  */
 int _abs(int n)
 {
