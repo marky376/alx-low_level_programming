623ccc9cf5a20f54143b1fbcc4ce9449eafd4fbe
marky376 <mmutuamark@gmail.com>
2024-02-11 23:53:48 +0300

    Adding comments

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,3 +1,12 @@
+/**
+ * pop_listint - function that removes the head node of a list
+ * @head: double pointer to the head node of the list
+ *
+ * This function removes the head node of a singly linked list and returns its value.
+ * If the list is empty, it returns 0.
+ *
+ * Return: the value of the removed head node, or 0 if the list is empty
+ */
 #include <stdio.h>
 #include "lists.h"
 #include <stdlib.h>
