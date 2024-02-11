d0f6b650a8c20773df721e1ab492aca15943a66a
marky376 <mmutuamark@gmail.com>
2024-02-11 23:55:58 +0300

    Adding comments

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,3 @@
-#include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
  * insert_nodeint_at_index - function that adds a new node in the list
  * depending on the index reference
@@ -9,44 +5,11 @@
  * @idx: The index to iterate the list
  * @n: The value in the new node
  *
- * Return: The new node created
+ * This function inserts a new node with the given value at the specified index
+ * in a singly linked list. If the index is 0, the new node becomes the new head
+ * of the list. If the index is greater than the number of nodes in the list,
+ * the function returns NULL and does not modify the list.
+ *
+ * Return: The new node created, or NULL if the index is out of range or if
+ * memory allocation fails.
  */
-
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
-{
-
-	listint_t *new_node = malloc(sizeof(listint_t));
-	listint_t *current = *head;
-	unsigned int counter = 0;
-
-	if (!new_node)
-	{
-		return (NULL);
-	}
-
-	new_node->n = n;
-
-	if (idx == 0)
-	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
-	}
-
-	while (current != NULL && counter < idx - 1)
-	{
-		current = current->next;
-		counter++;
-	}
-
-	if (counter < idx - 1)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	new_node->next = current->next;
-	current->next = new_node;
-
-	return (new_node);
-}
