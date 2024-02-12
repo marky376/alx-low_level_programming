77e7fcef01bf446eb3683064a176dc175de5de66
marky376 <mmutuamark@gmail.com>
2024-02-12 21:26:16 +0300

    Adding comments

--- 0x14-bit_manipulation/100-main.c
+++ 0x14-bit_manipulation/100-main.c
@@ -1,3 +1,12 @@
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function determines the endianness of the system
+ * by calling the get_endianness() function. If the system is little endian,
+ * it prints "Little Endian", otherwise it prints "Big Endian".
+ *
+ * Return: Always 0 (Success)
+ */
 #include <stdio.h>
 #include "main.h"
 
