d2318a76fd24a458090bcbbc7c1d4924a1b88fc4
marky376 <mmutuamark@gmail.com>
2024-02-12 21:26:50 +0300

    Adding comments

--- 0x14-bit_manipulation/5-main.c
+++ 0x14-bit_manipulation/5-main.c
@@ -10,13 +10,21 @@ int main(void)
 {
     unsigned int n;
 
+    // Call flip_bits function with arguments 1024 and 1
     n = flip_bits(1024, 1);
     printf("%u\n", n);
+
+    // Call flip_bits function with arguments 402 and 98
     n = flip_bits(402, 98);
     printf("%u\n", n);
+
+    // Call flip_bits function with arguments 1024 and 3
     n = flip_bits(1024, 3);
     printf("%u\n", n);
+
+    // Call flip_bits function with arguments 1024 and 1025
     n = flip_bits(1024, 1025);
     printf("%u\n", n);
+
     return (0);
 }
