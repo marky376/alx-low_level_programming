5c5ec2028dcf70bbfec650161e589cef5875f249
marky376 <mmutuamark@gmail.com>
2024-02-12 20:47:01 +0300

    Adding comments

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,13 +1,14 @@
-#include "main.h"
-
 /**
- * print_binary - function that  prints a binary number
- * @n: the integer  to be converted  to binary
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number to be converted to binary.
+ *
+ * Description: This function takes an unsigned long integer and prints its
+ *              binary representation. It recursively divides the number by 2
+ *              and prints the remainder until the number becomes 0.
+ *              The binary representation is printed in reverse order.
  */
-
 void print_binary(unsigned long int n)
 {
-
 	if (n > 1)
 	{
 		print_binary(n >> 1);
