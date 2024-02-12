37675662c88569cda8a9f77a182a650f3396c807
marky376 <mmutuamark@gmail.com>
2024-02-12 21:25:05 +0300

    Adding comments

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,24 +1,16 @@
-#include "main.h"
-
 /**
  * flip_bits - function that determines how many numbers
  * are required to be flipped to form another binary number
  * @n: the long integer to be used as case 1
  * @m: the long integer to be used as case 2
  *
+ * This function takes two long integers, n and m, and calculates
+ * the number of bits that need to be flipped in order to transform
+ * n into m. It performs a bitwise XOR operation between n and m
+ * to obtain a number with set bits at the positions where the bits
+ * in n and m differ. Then, it counts the number of set bits in the
+ * result using a bitwise AND operation and right shift. The final
+ * count is returned as the result.
+ *
  * Return: the number of bits to be flipped
  */
-
-unsigned int flip_bits(unsigned long int n, unsigned long int m)
-{
-	unsigned long int xor_prom = n ^ m;
-	unsigned int treck = 0;
-
-	while (xor_prom > 0)
-	{
-		treck += xor_prom & 1;
-		xor_prom >>= 1;
-	}
-
-	return (treck);
-}
