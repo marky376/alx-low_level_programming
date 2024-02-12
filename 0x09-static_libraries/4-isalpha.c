40b8b2a0f99438a5f0fb9d475a63c4e9080ee62b
marky376 <mmutuamark@gmail.com>
2024-02-12 20:43:58 +0300

    Adding comments

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -4,6 +4,10 @@
  * _isalpha - checks if a character is an alphabet
  * @c: the character to be checked
  *
+ * Description:
+ * This function takes a character as input and checks if it is an alphabet.
+ * It returns 1 if the character is an alphabet, and 0 otherwise.
+ *
  * Return: 1 if the character is an alphabet, 0 otherwise
  */
 int _isalpha(int c)
