c2b2bfb66c1e3ef9e97c18c071e1ecb949164b4c
marky376 <mmutuamark@gmail.com>
2024-02-14 05:56:24 +0300

    Sorting issues

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,5 +1,5 @@
-#include "main.h"
-#include <stdio.h>
+#include "main.h" // Include the header file "main.h"
+#include <stdio.h> // Include the standard input/output library
 
 /**
  * main - check the code
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	printf("%d\n", _strlen("My Dyn Lib"));
-	return (EXIT_SUCCESS);
+	printf("%d\n", _strlen("My Dyn Lib")); // Print the length of the string "My Dyn Lib"
+	return (EXIT_SUCCESS); // Return EXIT_SUCCESS to indicate successful execution
 }
