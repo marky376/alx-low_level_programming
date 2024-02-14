b8589f134dc35adaa56988d053e19f55ce74516d
marky376 <mmutuamark@gmail.com>
2024-02-14 05:58:39 +0300

    Sorting issues

--- 0x18-dynamic_libraries/100-tests.py
+++ 0x18-dynamic_libraries/100-tests.py
@@ -2,10 +2,17 @@ import random
 import ctypes
 
 cops = ctypes.CDLL('./100-operations.so')
-a = random.randint(-111, 111)
-b = random.randint(-111, 111)
-print("{} + {} = {}".format(a, b, cops.add(a, b)))
-print("{} - {} = {}".format(a, b, cops.sub(a, b)))
-print("{} x {} = {}".format(a, b, cops.mul(a, b)))
-print("{} / {} = {}".format(a, b, cops.div(a, b)))
-print("{} % {} = {}".format(a, b, cops.mod(a, b))
+
+def test_operations():
+    """
+    Test the operations defined in the dynamic library.
+    """
+    a = random.randint(-111, 111)
+    b = random.randint(-111, 111)
+    print("{} + {} = {}".format(a, b, cops.add(a, b)))
+    print("{} - {} = {}".format(a, b, cops.sub(a, b)))
+    print("{} x {} = {}".format(a, b, cops.mul(a, b)))
+    print("{} / {} = {}".format(a, b, cops.div(a, b)))
+    print("{} % {} = {}".format(a, b, cops.mod(a, b)))
+
+test_operations()
