318563779fbbd259913bdad8b57d9ac3d1f0d4ea
marky376 <mmutuamark@gmail.com>
2024-02-14 05:57:56 +0300

    Sorting issues

--- 0x18-dynamic_libraries/0-str_searching.c
+++ 0x18-dynamic_libraries/0-str_searching.c
@@ -11,7 +11,7 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int count = 0;
 
-	while (*s != '\0' && _strchr(accept, *s++) != NULL)
+	while (*s != '\0' && _strchr(accept, *s++) != NULL) // Iterate through the string until a character not in accept is found
 		count++;
 	return (count);
 }
@@ -27,7 +27,7 @@ char *_strpbrk(char *s, char *accept)
 {
 	while (*s != '\0')
 	{
-		if (_strchr(accept, *s) != NULL)
+		if (_strchr(accept, *s) != NULL) // Iterate through the string until a character in accept is found
 			return (s);
 		s++;
 	}
@@ -48,17 +48,17 @@ char *_strstr(char *haystack, char *needle)
 		char *start = haystack;
 		char *pattern = needle;
 
-		while (*haystack != '\0' && *pattern != '\0' && *haystack == *pattern)
+		while (*haystack != '\0' && *pattern != '\0' && *haystack == *pattern) // Iterate through the string and the pattern until a mismatch is found
 		{
 			haystack++;
 			pattern++;
 		}
 
-		if (*pattern == '\0')
+		if (*pattern == '\0') // If the pattern is fully matched, return the start pointer
 			return (start);
 
-		haystack = start + 1;
+		haystack = start + 1; // Move the haystack pointer to the next character
 	}
 
-	return (NULL);
+	return (NULL); // Return NULL if the substring is not found
 }
