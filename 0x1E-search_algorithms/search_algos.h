59e218c99ffc822264faee16bb5538f21672e815
marky376 <mmutuamark@gmail.com>
2024-04-10 01:14:38 +0300

    A commit on some files to work on

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -24,7 +24,23 @@ typedef struct listint_s
     size_t index;
     struct listint_s *next;
 } listint_t;
-
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
 
 #endif
 
